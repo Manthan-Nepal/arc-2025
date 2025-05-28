1679c3ae8939acb0b6a04e99e0ad8bde05144ece
Reyozaki <salonngakhushi2077@kct.edu.np>
2025-05-28 16:53:36 +0545

    Add Daily Task: Functions and Modules, and some updates to previous files.

---
+++ arc/backend/salon-ngakhushi/functions-and-modules/ImportcontactBook.py
@@ -0,0 +1,21 @@
+# import sys
+# sys.path.insert(1, 'D:\Programming\ARC_2025\arc-2025\arc\backend\salon-ngakhushi\python-data-structures\tasks')
+# import file
+
+# import sys
+# sys.path.append('../')
+# import ContactBook as modu
+
+import contactBookModule as modu
+
+print("\n", modu.ContactBook1.get("Name"))
+
+print("\nSlicing: ", modu.ContactBook1["Name"][1:-1])
+
+modu.ContactBook1["Name"].insert(2, 'Denji')
+print("\n", modu.ContactBook1.get("Name"))
+
+print("\nInitial Hiro: ", modu.ContactBook2.get("Hiro"))
+
+modu.ContactBook2.update({"Hiro":{"phone": 9864234412, "email": "hiroyarika@yahoo.mail"}})
+print("\nUpdated Hiro: ", modu.ContactBook2.get("Hiro"))
