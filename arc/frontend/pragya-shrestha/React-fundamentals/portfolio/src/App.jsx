d615d4512b47aa97a41aba808df3627585080bde
pragya-02 <shresthapragya961@gmail.com>
2025-06-16 09:15:31 +0545

    feat: interactive todo app

--- arc/frontend/pragya-shrestha/React-fundamentals/portfolio/src/App.jsx
+++ arc/frontend/pragya-shrestha/React-fundamentals/portfolio/src/App.jsx
@@ -1,25 +1,23 @@
-import About from './components/About';
-import Contact from './components/Contact';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 function App() {
-  return(
+  return (
     <>
-    <Header/>
-    <Hero/>
+      <Header />
+      <Hero />
       <About />
       <Projects />
       <Experience />
       <Contact />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ arc/frontend/pragya-shrestha/React-fundamentals/to-do app/src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  )
+}
